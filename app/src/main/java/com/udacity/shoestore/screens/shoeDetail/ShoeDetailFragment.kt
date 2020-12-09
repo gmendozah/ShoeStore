d36731835d5056df8df4fbb62d7330fa4acdef29
Geovani <geovanimendoza.h@gmail.com>
2020-12-09 19:00:06 -0400

    refactor: navigation and show detail

--- app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeDetailFragmentBinding
 
@@ -13,15 +14,19 @@ class ShoeDetailFragment : Fragment() {
     private lateinit var viewModel: ShoeDetailViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.shoe_detail_fragment, container, false)
         viewModel = ViewModelProvider(this).get(ShoeDetailViewModel::class.java)
-
         setHasOptionsMenu(true)
+
+
+        binding.cancelButton.setOnClickListener { navigateToShoeList(it) }
         return binding.root
     }
+
+    private fun navigateToShoeList(view: View) = view.findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListingFragment())
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         menu.clear()
