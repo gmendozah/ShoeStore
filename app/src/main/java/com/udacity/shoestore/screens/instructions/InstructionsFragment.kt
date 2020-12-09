ced066043487e693d66f41b5bd7b122d997e79fb
Geovani <geovanimendoza.h@gmail.com>
2020-12-09 11:22:54 -0400

    feat: implement data binding on welcome and instruction screens
    - add navigation implementation

--- app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val binding: FragmentInstructionsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        binding.instructionsNextBtn.setOnClickListener { navigateToShoeListingScreen(it) }
+        return binding.root
     }
+
+    private fun navigateToShoeListingScreen(view: View) = view.findNavController()
+            .navigate(InstructionsFragmentDirections.actionInstructionsFragmentToShoeListingFragment())
 }
--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        binding.welcomeNextBtn.setOnClickListener { navigateToInstructionsScreen(it) }
+        return binding.root
     }
+
+    private fun navigateToInstructionsScreen(view: View) = view.findNavController()
+            .navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
 }
