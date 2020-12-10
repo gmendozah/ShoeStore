91da53d5b8145047e36f754fc51c68c87ab007b6
Geovani <geovanimendoza.h@gmail.com>
2020-12-09 23:30:55 -0400

    refactor: add form valdiation

--- app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
@@ -3,6 +3,7 @@ package com.udacity.shoestore.screens.shoeDetail
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -23,7 +24,34 @@ class ShoeDetailFragment : Fragment() {
 
 
         binding.cancelButton.setOnClickListener { navigateToShoeList(it) }
+        binding.saveButton.setOnClickListener { validateForm() }
         return binding.root
+    }
+
+    private fun validateForm(): Boolean {
+        when {
+            binding.nameEditText.text.isEmpty() -> {
+                Toast.makeText(context, R.string.nameValidation, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            binding.sizeEditText.text.isEmpty() -> {
+                Toast.makeText(context, R.string.sizeValidation, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            binding.companynEditText.text.isEmpty() -> {
+                Toast.makeText(context, R.string.companyValidation, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            binding.descriptionEditText.text.isEmpty() -> {
+                Toast.makeText(context, R.string.descriptionValidation, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            binding.sizeEditText.text.toString().toDouble() <= 0 -> {
+                Toast.makeText(context, R.string.sizeValidationValue, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            else -> return true
+        }
     }
 
     private fun navigateToShoeList(view: View) = view.findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListingFragment())
