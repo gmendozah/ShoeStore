99ad2d4ea412e9b6a1e7501da7d1ad2a9db4e52f
Geovani <geovanimendoza.h@gmail.com>
2020-12-11 23:00:21 -0400

    refactor: refactor listing

--- app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingFragment.kt
@@ -1,23 +1,20 @@
 package com.udacity.shoestore.screens.shoeListing
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeListingFragmentBinding
-import com.udacity.shoestore.models.Shoe
-import timber.log.Timber
 
 class ShoeListingFragment : Fragment() {
 
     private lateinit var binding: ShoeListingFragmentBinding
-    private lateinit var viewModel: ShoeListingViewModel
-    private lateinit var viewModelFactory: ShoeListingViewModelFactory
+    private val viewModel: ShoeListingViewModel by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -25,29 +22,26 @@ class ShoeListingFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.shoe_listing_fragment, container, false)
         val shoeListingFragmentArgs by navArgs<ShoeListingFragmentArgs>()
-        viewModelFactory = ShoeListingViewModelFactory(shoeListingFragmentArgs.shoeList)
-
-        viewModel = ViewModelProvider(this, viewModelFactory).get(ShoeListingViewModel::class.java)
-
+        if (shoeListingFragmentArgs.shoe != null) {
+            viewModel.addShoe(shoeListingFragmentArgs.shoe!!)
+        }
         binding.shoeListingViewModel = viewModel
         binding.lifecycleOwner = this
 
         viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoeList ->
-            Timber.i(shoeList.toString())
             var listText = ""
             for (shoe in shoeList) {
                 listText += "Name: ${shoe.name}\nSize: ${shoe.size}\nCompany: ${shoe.company}\nDescription: ${shoe.name}\n\n"
             }
             binding.textItemList.text = listText
-            Timber.e(listText)
         })
 
 
         binding.shoeListNextButton.setOnClickListener {
-            navigateToDetailScreen(viewModel.shoeList.value ?: emptyArray())
+            navigateToDetailScreen()
         }
         return binding.root
     }
 
-    private fun navigateToDetailScreen(shoeList: Array<Shoe>) = findNavController().navigate(ShoeListingFragmentDirections.actionShoeListingFragmentToShoeDetailFragment(shoeList))
+    private fun navigateToDetailScreen() = findNavController().navigate(ShoeListingFragmentDirections.actionShoeListingFragmentToShoeDetailFragment())
 }
--- app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingViewModel.kt
@@ -7,21 +7,22 @@ import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.models.Shoe
 import timber.log.Timber
 
-class ShoeListingViewModel(shoeList: Array<Shoe>?) : ViewModel() {
+class ShoeListingViewModel : ViewModel() {
     private val _shoeList = MutableLiveData<MutableList<Shoe>>()
-    val shoeList: LiveData<Array<Shoe>> = Transformations.map(_shoeList) {
-        it.toTypedArray()
-    }
+    val shoeList: LiveData<MutableList<Shoe>>
+        get() = _shoeList
 
     init {
-        if (shoeList.isNullOrEmpty()) {
+        if (_shoeList.value.isNullOrEmpty()) {
             _shoeList.value = mutableListOf(
                     Shoe(name = "Shoe 1", size = 9.5, company = "Company 1", description = "Description 1"),
                     Shoe(name = "Shoe 2", size = 10.5, company = "Company 2", description = "Description 2"),
                     Shoe(name = "Shoe 3", size = 8.5, company = "Company 3", description = "Description 3"),
             )
-        } else {
-            _shoeList.value = shoeList.toMutableList()
         }
+    }
+
+    fun addShoe(shoe: Shoe) {
+        _shoeList.value?.add(shoe)
     }
 }
